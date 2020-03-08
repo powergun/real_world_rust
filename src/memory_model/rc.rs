b2e162fdf3cc0f76bf830f124dfd6ee380fc06ab
Wei Ning <wei.ning@digitalasset.com>
2020-03-09 10:16:14 +1100

    wip

--- src/memory_model/rc.rs
+++ src/memory_model/rc.rs
@@ -2,9 +2,12 @@
 use std::rc::Rc;
 
 fn _demo_rc_creation() {
-    let mut s = String::from("thereisacow");
+    let s = String::from("thereisacow");
     {
         let ptr1 = Rc::new(s);
+        // I originally used this to demo the point
+        // let mut ptr1 = Rc::new(s);  but rust compiler and IDE warns that
+        // ptr1 does not need to be mutable
         // the content of s is now owned by ptr1
         // put the content is immut due to
         // multiple borrowing rule
