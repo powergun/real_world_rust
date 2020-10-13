67ca4745198f3f215f64e3b8338db1b8987c5f2d
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-10-13 20:06:51 +1100

    more vector

--- src/lib/containers/vectors/split_concat.rs
+++ src/lib/containers/vectors/split_concat.rs
@@ -1,1 +1,15 @@
 
+// source
+// rust std lib cookbook P/49
+
+#[test]
+fn demo_split_first() {
+    let xs = vec![1, 2, 3, 4, 5];
+
+    if let Some((first, xs_)) = xs.split_first() {
+        assert_eq!(first, &1);
+        assert_eq!(4, xs_.len());
+    } else {
+        panic!("shall not fail!");
+    }
+}
