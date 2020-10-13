00865eb888fabdc064cea3c4d91b90144076a328
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-10-13 16:30:38 +1100

    more environment variable

--- src/lib/system_programming/environment/read_env.rs
+++ src/lib/system_programming/environment/read_env.rs
@@ -1,15 +1,27 @@
 // see also bin/cat.rs for another simple example
 
+// also explained in:
+// rust std lib cookbook P/38
+
+#[allow(unused_imports)]
 use std::env;
 
-fn _demo_read_env_var() {
+#[test]
+fn demo_iterate_over_env_vars() {
     let vars = env::vars();
-    for (k, v) in vars {
-        println!("{}={}", k, v);
+    for (env_var_name, _env_var_value) in vars {
+        assert_ne!("asd", env_var_name);
     }
 }
 
 #[test]
-fn demo_all() {
-    _demo_read_env_var();
+fn demo_safe_read_one_env_var() {
+    let f = |name: String| -> String {
+        match env::var(name) {
+            Ok(val) => val,
+            Err(_) => String::new(),
+        }
+    };
+    assert_eq!("", f("asdasd".to_string()));
+    assert_eq!("/bin/bash", f("SHELL".to_string()));
 }
