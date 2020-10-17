c2b3377225c08f1cc1514e44cf03868ef09dba62
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-10-17 14:40:46 +1100

    more iter

--- src/lib/fp/iterators/enumerate.rs
+++ src/lib/fp/iterators/enumerate.rs
@@ -1,6 +1,9 @@
+// no C-style for loop!!!
+
 #[test]
 pub fn test_enumerate_iterator() {
     // L431
     let o: Vec<usize> = (1..4).enumerate().map(|pair| pair.0).collect();
     assert_eq!(vec![0, 1, 2], o);
+
 }
