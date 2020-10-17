64d766acbb58844b968260b6b6d008429c6973c0
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-10-17 15:25:49 +1100

    more deque

--- src/lib/containers/mod.rs
+++ src/lib/containers/mod.rs
@@ -1,14 +1,10 @@
 pub mod array;
 pub mod hashmap;
 pub mod iterators;
-
 pub mod doubly_linkedlist;
 pub mod singly_linkedlist;
-
 pub mod linkedlist;
-
 pub mod slices;
-
 pub mod tuples;
-
 pub mod vectors;
+pub mod queues;
---
+++ src/lib/containers/queues/vecdeque.rs
@@ -0,0 +1,30 @@
+// rust std lib cookbook P/67
+// vec_deque has capacity
+// internally vec deque uses a ring buffer
+
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+
+#[test]
+fn demo_create_deque() {
+    // vecdeque is a deque, which is FIFO
+    let mut orders: VecDeque<String> = VecDeque::new();
+    orders.push_back("value: T".to_string());
+}
+
+#[test]
+fn demo_safe_pop_front() {
+    let mut orders: VecDeque<String> = VecDeque::new();
+    assert!(orders.pop_front().is_none());
+}
+
+#[test]
+fn demo_swap_remove_both_ends() {
+    let mut orders = VecDeque::new();
+    (1..5).for_each(|x| orders.push_back(x));  // 1, 2, 3, 4
+    orders.swap_remove_back(0);
+    assert_eq!(4, orders[0]);  // 4, 2, 3
+
+    orders.swap_remove_front(1);
+    assert_eq!(4, orders[0]);  // 4, 3
+}
