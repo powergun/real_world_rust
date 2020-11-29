75a0c23e87c53dfd5a6d1206003202eca96045b2
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-11-29 17:40:37 +1100

    wip

--- src/lib/con_par/mutex.rs
+++ src/lib/con_par/mutex.rs
@@ -1,4 +1,5 @@
-use std::thread;
+#[allow(unused_imports)]
+use std::thread::spawn;
 // algorithms with rust L776
 // in order to provide that ownership across threads - similar
 // to what Rc does within a single thread - rust provides the
@@ -6,26 +7,37 @@ use std::thread;
 // Using this mutex on top, it is the thread-safe equivalent
 // of an Rc wrapping a RefCell, a reference counter that wraps
 // a mutable container.
+#[allow(unused_imports)]
 use std::sync::{Arc, Mutex};
 
-pub fn demo_mutex() {
-    let v = Arc::new(Mutex::new(vec![]));
-    let handles = (0..10).map(|i| {
-        let numbers = Arc::clone(&v);
-        thread::spawn(move || {
-            let mut vector = numbers.lock().unwrap();
-            (*vector).push(i);
-        })
-    });
-
-    for handle in handles {
-        handle.join().unwrap();
+#[test]
+fn demo_mutex() {
+    // rust enforces mutex locking by requesting the data source
+    // be wrapped in Arc & Mutex
+    let store_with_mutex = Arc::new(Mutex::new(Vec::<i32>::new()));
+    let mut handles = Vec::with_capacity(10);
+    for i in 0..10 {
+        let write_lock = store_with_mutex.clone();
+        let index = i; // can not move i directly to the thread
+        handles.push(spawn(move || {
+            // lock() is a method on Mutex but Arc (like RC)
+            // allows caller to invoke method on the wrapped
+            // type directly.
+            // lock() returns a Result type;
+            // the only time Err(e) can happen is when another
+            // thread that holds the lock panics - it is called
+            // mutex-poisoning;
+            // unwrap() is more commonly used in this situation
+            let mut p = write_lock.lock().unwrap();
+            p.push(index);
+        }));
     }
 
-    println!("{:?}", *v.lock().unwrap());
-}
+    // consume and join all threads
+    handles
+        .into_iter()
+        .for_each(|hd| hd.join().expect("fail to join"));
 
-#[test]
-fn demo_all() {
-    demo_mutex();
+    let store = store_with_mutex.lock().unwrap();
+    assert_eq!(store.len(), 10);
 }
