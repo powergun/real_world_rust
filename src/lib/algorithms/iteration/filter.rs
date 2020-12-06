b7365b74285d37896608175f974474710e7c83c8
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-06 18:25:18 +1100

    wip

--- src/lib/algorithms/iteration/filter.rs
+++ src/lib/algorithms/iteration/filter.rs
@@ -1,17 +1,25 @@
-// recall grep {} in perl and ruby
-pub fn iterator_filter() {
+#[test]
+fn demo_iterator_filter() {
+    // recall grep {} in perl and ruby
+
     let nums = vec![3, 1, 4, 1, 5, 9];
-    let larger_then_three = nums.into_iter().filter(|&x| x > 3);
-    larger_then_three.for_each(|elem| print!("{}, ", elem));
-    println!("");
+    let larger_then_three = nums
+        .into_iter()
+        .filter(|&x| x > 3)
+        .collect::<Vec<i32>>();
+    assert_eq!(larger_then_three, vec![4, 5, 9]);
 
     // collect the result in a vec
     // Vec<_> don't care about the type
     let items: Vec<_> = (1..10).filter(|&x| x > 3).collect();
-    println!("{:?}", items);
+    assert_eq!(items, vec![4, 5, 6, 7, 8, 9]);
 }
 
 #[test]
-fn demo_iterator_filter() {
-    iterator_filter();
+fn demo_iteration_using_range() {
+    // 10..1 produces an empty range
+    for _ in 10..1 {
+        println!("asd");
+    }
+    assert!((10..1).collect::<Vec<_>>().is_empty());
 }
--- src/lib/algorithms/iteration/map.rs
+++ src/lib/algorithms/iteration/map.rs
@@ -1,20 +1,17 @@
-// recall map {} in perl and ruby
-pub fn iterator_map() {
-    let nums = vec![3, 1, 4, 1, 5, 9];
-    let operand = 1;
-    nums.iter()
-        .map(|x| x + operand)
-        .for_each(|x| print!("{}, ", x));
-    // map() is a closure; it can use the variables in the
-    // calling scope; same applies to filter()
-    println!("");
-
-    // the original vector is unmodified
-    nums.iter().for_each(|elem| print!("{}, ", elem));
-    println!("");
-}
-
 #[test]
 fn demo_iterator_map() {
-    iterator_map();
+    // recall map {} in perl and ruby
+
+    let nums = vec![3, 1, 4, 1, 5, 9];
+    let operand = 1;
+    let xs: Vec<_> = nums.iter()
+        .map(|x| x + operand)
+        .collect();
+    // map() is a closure; it can use the variables in the
+    // calling scope; same applies to filter();
+    // there are different accessing mode: copy and move
+    assert_eq!(xs, vec![4, 2, 5, 2, 6, 10]);
+
+    // the original vector is unmodified, of course
+    assert_eq!(nums, vec![3, 1, 4, 1, 5, 9]);
 }
