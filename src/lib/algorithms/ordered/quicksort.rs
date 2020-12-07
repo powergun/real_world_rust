3cf067832ec56120b20efadcc48cd67fa0dfc487
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-07 12:03:00 +1100

    wip

--- src/lib/algorithms/ordered/quicksort.rs
+++ src/lib/algorithms/ordered/quicksort.rs
@@ -51,6 +51,42 @@ fn quick_sort_rand<T: PartialOrd + Debug>(xs: &mut Vec<T>) {
     f(xs, 0, xs.len(), &mut rng);
 }
 
+#[allow(dead_code)]
+fn quick_sort_rayon<T: PartialOrd + Send>(xs: &mut Vec<T>) {
+    // use rayon::join() to distribute the workload to multiple threads;
+    // recall TBB's parallel_invoke() function
+    // rayon::join() puts the second computation to 
+    fn f<T: PartialOrd + Send>(xs: &mut [T]) {
+        if xs.len() > 1 {
+            let p = pivot(xs);
+            let (a, b) = xs.split_at_mut(p);
+            rayon::join(
+                || f(a),
+                || f(&mut b[1..]),
+            );
+        }
+    }
+
+    fn pivot<T: PartialOrd + Send>(xs: &mut [T]) -> usize {
+        if xs.len() > 1 {
+            let mut p = xs.len();
+            for i in 1..xs.len() {
+                if xs[i] > xs[0] {
+                    p -= 1;
+                    xs.swap(i, p);
+                }
+            }
+            p -= 1;
+            xs.swap(p, 0);
+            p
+        } else {
+            0
+        }
+    }
+
+    f(xs);
+}
+
 #[test]
 fn demo_quick_sort() {
     {
@@ -100,6 +136,30 @@ fn demo_quick_sort_random_pivot() {
 }
 
 #[test]
+fn demo_quick_sort_rayon() {
+    {
+        let mut xs: Vec<_> = (1..13).rev().collect();
+        quick_sort_rayon(&mut xs);
+        assert_eq!((1..13).collect::<Vec<i32>>(), xs);
+    }
+    {
+        let mut xs: Vec<i32> = vec![];
+        quick_sort_rayon(&mut xs);
+        assert!(xs.is_empty());
+    }
+    {
+        let mut xs: Vec<i32> = vec![1];
+        quick_sort_rayon(&mut xs);
+        assert_eq!(vec![1], xs);
+    }
+    {
+        let mut xs: Vec<i32> = vec![3, 1, 4];
+        quick_sort_rayon(&mut xs);
+        assert_eq!(vec![1, 3, 4], xs);
+    }
+}
+
+#[test]
 fn performance_comparison() {
     // 10 loops: 50.7381604 ms
     // 10 loops: 35.7711494 ms
@@ -114,6 +174,12 @@ fn performance_comparison() {
         let mut xs: Vec<_> = (1..1000).rev().collect();
         timeit!({
             quick_sort_rand(&mut xs);
+        });
+    }
+    {
+        let mut xs: Vec<_> = (1..1000).rev().collect();
+        timeit!({
+            quick_sort_rayon(&mut xs);
         });
     }
 }
